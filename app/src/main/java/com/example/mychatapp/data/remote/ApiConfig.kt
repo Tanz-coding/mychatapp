071ce6613e5135396e7a867f14d04bb64eb7565b
Yuting Tan <3156212493@qq.com>
2026-05-23 12:56:12 +0800

    feat: add api config

---
+++ app/src/main/java/com/example/mychatapp/data/remote/ApiConfig.kt
@@ -0,0 +1,10 @@
+package com.example.mychatapp.data.remote
+
+object ApiConfig {
+    /**
+     * Android emulator can use 10.0.2.2 to access the host machine.
+     * Replace it with your LAN server address when testing on a real phone.
+     */
+    const val DEFAULT_BASE_URL = "http://10.0.2.2:3000/"
+    const val DEFAULT_SOCKET_URL = "http://10.0.2.2:3000"
+}
